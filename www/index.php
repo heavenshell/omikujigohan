62b63a811c3041dc5f0c7209837ae93827679408
heavenshell <heavenshell.jp@gmail.com>
2011-12-31 15:56:25 +0900

    Bump up copyright.

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5.3
  *
- * Copyright (c) 2011 Shinya Ohyanagi, All rights reserved.
+ * Copyright (c) 2011-2012 Shinya Ohyanagi, All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,7 +39,7 @@
  * @package   \Omikujigohan
  * @version   $id$
  * @copyright (c) https://github.com/woopsdez
- * @copyright (c) 2011 Shinya Ohyanagi
+ * @copyright (c) 2011-2012 Shinya Ohyanagi
  * @author    Shinya Ohyanagi <sohyanagi@gmail.com>
  * @license   New BSD License
  * @link      https://github.com/woopsdez
@@ -52,7 +52,7 @@ namespace Omikujigohan;
  * @category  \Omikujigohan
  * @package   \Omikujigohan
  * @version   $id$
- * @copyright (c) 2011 Shinya Ohyanagi
+ * @copyright (c) 2011-2012 Shinya Ohyanagi
  * @author    Shinya Ohyanagi <sohyanagi@gmail.com>
  * @license   New BSD License
  */
@@ -68,7 +68,7 @@ namespace Omikujigohan\Model;
  * @category  \Omikujigohan
  * @package   \Omikujigohan
  * @version   $id$
- * @copyright (c) 2011 Shinya Ohyanagi
+ * @copyright (c) 2011-2012 Shinya Ohyanagi
  * @author    Shinya Ohyanagi <sohyanagi@gmail.com>
  * @license   New BSD License
  */
